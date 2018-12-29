7449e53fd22d5c689314e4b1e1100a1b1ddfad33
Sven Olding <sven.olding@gmx.de>
2018-12-29 12:24:25 +0100

    ninject object scope example

--- EssentialTools/Controllers/HomeController.cs
+++ EssentialTools/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace EssentialTools.Controllers
 
         private Product[] products = { new Product { Name = "Kayak", Category = "Watersports", Price = 275M }, new Product { Name = "Lifejacket", Category = "Watersports", Price = 48.95M }, new Product { Name = "Soccer ball", Category = "Soccer", Price = 19.50M }, new Product { Name = "Corner flag", Category = "Soccer", Price = 34.95M } };
 
-        public HomeController(IValueCalculator calcParam)
+        public HomeController(IValueCalculator calcParam, IValueCalculator calc2)
         {
             calc = calcParam;
         }
--- EssentialTools/Models/LinqValueCalculator.cs
+++ EssentialTools/Models/LinqValueCalculator.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 
 namespace EssentialTools.Models
 {
     public class LinqValueCalculator : IValueCalculator
     {
+        private static int counter = 0;
+
         private IDiscountHelper discounter;
 
         public LinqValueCalculator(IDiscountHelper discountParam)
         {
             discounter = discountParam;
+            Debug.WriteLine(string.Format("Instance {0} created", ++counter));
         }
 
         public decimal ValueProducts(IEnumerable<Product> products)
