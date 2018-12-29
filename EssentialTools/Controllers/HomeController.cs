de9e3d69dbb9e96d25da63c9ee74b3f333beddea
Sven Olding <sven.olding@gmx.de>
2018-12-29 11:20:44 +0100

    add dependency resolver

--- EssentialTools/Controllers/HomeController.cs
+++ EssentialTools/Controllers/HomeController.cs
@@ -8,16 +8,18 @@ namespace EssentialTools.Controllers
 {
     public class HomeController : Controller
     {
+        private IValueCalculator calc;
+
         private Product[] products = { new Product { Name = "Kayak", Category = "Watersports", Price = 275M }, new Product { Name = "Lifejacket", Category = "Watersports", Price = 48.95M }, new Product { Name = "Soccer ball", Category = "Soccer", Price = 19.50M }, new Product { Name = "Corner flag", Category = "Soccer", Price = 34.95M } };
+
+        public HomeController(IValueCalculator calcParam)
+        {
+            calc = calcParam;
+        }
 
         // GET: Home
         public ActionResult Index()
         {
-            IKernel ninjectKernel = new StandardKernel();
-            ninjectKernel.Bind<IValueCalculator>().To<LinqValueCalculator>();
-
-            IValueCalculator calc = ninjectKernel.Get<IValueCalculator>();
-
             ShoppingCart cart = new ShoppingCart(calc) { Products = products };
             decimal totalValue = cart.CalculateProductTotal();
 
